1825471542bb423489c69139fbc292b164d85b19
Viridiana Ducoing <viridiana@ducoing.mx>
2025-02-22 15:41:42 -0300

    clean

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,12 @@
 import { Component } from '@angular/core';
-//import { BinaryTreeComponent } from './binary-tree/binary-tree.component';
 import { DaxcsaTreeComponent } from './daxcsa-tree/daxcsa-tree.component';
 
 @Component({
   selector: 'app-root',
   imports: [DaxcsaTreeComponent],
-  /*templateUrl: './app.component.html',*/
-  /*template: `
-    <h1>Daxcsa Binary Tree (Standalone Angular)</h1>
-    <app-binary-tree></app-binary-tree>
-  `,*/
   standalone: true,
-  /*styleUrl: './app.component.css'*/
   template: `
     <div class="app-container">
-      <!-- The tree component is used here -->
       <daxcsa-tree></daxcsa-tree>
     </div>
   `,
--- src/app/d3-tree.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { D3TreeDirective } from './d3-tree.directive';
-
-describe('D3TreeDirective', () => {
-  it('should create an instance', () => {
-    const directive = new D3TreeDirective();
-    expect(directive).toBeTruthy();
-  });
-});
